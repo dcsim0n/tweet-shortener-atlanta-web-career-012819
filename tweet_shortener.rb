c45d972b17c43aae84cc80ae1a278321fd0d2197
Dana Simmons <dsanalog@gmail.com>
2019-01-22 22:06:10 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,39 @@
 # Write your code here.
+
+dictionary = {
+  "hello" => 'hi',
+  "to" => '2',
+  "two" => '2',
+  "too" => '2',
+  "for" => '4',
+  "four" => '4',
+  "be" => 'b',
+  "you" => 'b',
+  "at" => '@',
+  "and" => '&'
+  
+}
+
+def word_substituter(tweet)
+  words = tweet.split()
+  new_tweet = words.map do |w|
+    if dictionary.keys.include?(w)
+      return dictionary[w]
+    else
+      return w
+    end
+  end #end map
+end
+
+def bulk_twee_shortener(tweet_list)
+  
+  puts("Final result")
+end
+
+def selective_tweet_shortener(tweet)
+  
+end
+
+def shortened_tweet_truncator(tweet)
+  #add an ellipsis if the twee is too long!
+end
